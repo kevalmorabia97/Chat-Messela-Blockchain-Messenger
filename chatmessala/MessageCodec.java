dd0c79bda89c73a37949456a2ce25994445c294a
kevalmorabia97 <kevalmorabia97@gmail.com>
2018-03-31 19:40:30 +0530

    skeleton code written

---
+++ chatmessala/MessageCodec.java
@@ -0,0 +1,13 @@
+package chatmessala;
+
+public class MessageCodec {
+	static String publicKey = "publicKey";
+	
+	public static String encrypt(String plainText, String privateKey) {
+		return "cipherText";
+	}
+	
+	public static String decrypt(String cipherText, String privateKey) {
+		return "plainText";
+	}
+}
