b495dcd03a76c16c21707b5d12afda47fe956cdb
kevalmorabia97 <kevalmorabia97@gmail.com>
2018-04-12 14:47:19 +0530

    beautify2

--- Main.java
+++ Main.java
@@ -4,8 +4,10 @@ public class Main {
 		keval.broadcastPublicKey();
 		keval.start();
 
-		Thread.sleep(5000);
+		Thread.sleep(10000);
 
+		System.out.println("SENDING MESSAGES");
+		
 		keval.createMessage("I am keval", "keval");
 		Thread.sleep(5000);
 		keval.createMessage("hey guys", "addy");
@@ -14,8 +16,45 @@ public class Main {
 		Thread.sleep(5000);
 
 		keval.printMyMessages();
-		//		
-		//		Miner pro = new Miner("pro", 1111);
-		//		pro.start();
+		
+//		User addy = new User("addy", 1111);
+//		addy.broadcastPublicKey();
+//		addy.start();
+//
+//		Thread.sleep(10000);
+//
+//		System.out.println("SENDING MESSAGES");
+//		
+//		addy.createMessage("I am addy", "kriti");
+//		Thread.sleep(5000);
+//		addy.createMessage("hey ppl", "addy");
+//		Thread.sleep(5000);
+//		addy.createMessage("noobie", "keval");
+//		Thread.sleep(5000);
+//
+//		addy.printMyMessages();
+//		
+		
+//		User kriti = new User("kriti", 1111);
+//		kriti.broadcastPublicKey();
+//		kriti.start();
+//
+//		Thread.sleep(10000);
+//
+//		System.out.println("SENDING MESSAGES");
+//
+//		kriti.createMessage("I am kriti", "addy");
+//		Thread.sleep(5000);
+//		kriti.createMessage("hello", "kriti");
+//		Thread.sleep(5000);
+//		kriti.createMessage("hahaha", "keval");
+//		Thread.sleep(5000);
+//
+//		kriti.printMyMessages();
+//		
+		
+//		
+//		Miner pro = new Miner("pro", 1111);
+//		pro.start();
 	}
 }
